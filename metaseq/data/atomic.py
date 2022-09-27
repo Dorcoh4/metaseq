b59884d969d1edcb2193870fba90ccfdd7faade3
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-09-27 11:58:55 +0300

    Create atomic.py

---
+++ metaseq/data/atomic.py
@@ -0,0 +1,10 @@
+
+def atomic_read():
+    print("ERROR: THE ATOMS ARE ALL FAKEEEE")
+    raise Exception("THE ATOMS ARE ALL FAKEEEE")
+def atomic_write():
+    print("ERROR: THE ATOMS ARE ALL FAKEEEE")
+    raise Exception("THE ATOMS ARE ALL FAKEEEE")
+def atomic_read_all():
+    print("ERROR: THE ATOMS ARE ALL FAKEEEE")
+    raise Exception("THE ATOMS ARE ALL FAKEEEE")
